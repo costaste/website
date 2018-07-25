2d775c9dd1f1d9ffc00a3a8ed8b08948d1532ac1
Stephen Costa <st3ph3n24@gmail.com>
2018-07-25 10:52:58 -0400

    Change theme to dark grey/wheat combo

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -7,7 +7,7 @@ class HomePage extends Component {
         <br/>
         <h1>Hello World!</h1>
         <br/>
-        <img src={require('../../assets/images/placeholder.jpg')} width='250px' style={{borderRadius:'25px'}}/>
+        <img src={require('../../assets/images/placeholder.jpg')} className='profileImg'/>
         <h3>I'm Stephen Costa, a computer science student studying at Northeastern University in Boston. </h3>
         <br/>
         <ul class='list'>
